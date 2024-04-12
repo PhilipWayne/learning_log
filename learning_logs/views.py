76d875ab4e79b8b67be5d5cbe6aad472c4dc99d4
Philip Svyshchov <cash99tan@gmail.com>
2024-04-12 00:21:16 -0600

    get object or 404 implemented

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -25,7 +25,7 @@ def topics(request):
 @login_required
 def topic (request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure the topic belongs to the current user.
     if topic.owner != request.user:
         raise Http404
@@ -115,8 +115,4 @@ def my_view(request):
         return HttpResponse("Error: Invalid form instance")
     
     return render(request, 'my_template.html', {'form': form})
-
-def custom_404(request, exception):
-    print("Custom 404 function called.")
-    return render(request, '404.html', status=404)
 
